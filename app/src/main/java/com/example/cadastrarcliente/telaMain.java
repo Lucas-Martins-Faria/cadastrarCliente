c31140f0327a242f9290f940a4ea695ee9922edf
Lucas Faria <Lucas1999faria@gmail.com>
2021-10-19 22:02:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cadastrarcliente/telaMain.java
@@ -0,0 +1,14 @@
+package com.example.cadastrarcliente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.telaMain);
+    }
+}
